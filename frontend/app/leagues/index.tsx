ddee5fcec2e7fa068ad8aeb5bb6728c4f000d6cf
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2023-07-30 17:41:50 +0000

    mustica pro font added

--- frontend/app/leagues/index.tsx
+++ frontend/app/leagues/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { Link, router } from 'expo-router';
 import { styles } from '../../styles/styles';
 
@@ -7,6 +7,10 @@ export default function Page() {
   return (
     <View style={styles.container}>
       <Link href="/leagues/testId">Go to league</Link>
+      <Text style={{ fontFamily: 'MusticaPro', color: '#fff', fontSize: 30 }}>
+        The mustica Pro Font
+      </Text>
+      <Text style={{ color: '#fff', fontSize: 30 }}>RegularFont</Text>
       <Button
         title="go to updated league"
         onPress={() => {
