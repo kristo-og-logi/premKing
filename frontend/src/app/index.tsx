bcd49c1f545da8594787385a19d9b1f0549c053a
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2023-08-01 17:26:56 +0000

    fixing import path error

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -4,7 +4,7 @@ import { useFonts } from 'expo-font';
 
 export default function Page() {
   const [fontsLoaded] = useFonts({
-    MusticaPro: require('../assets/MusticaPro-SemiBold.otf'),
+    MusticaPro: require('../../assets/MusticaPro-SemiBold.otf'),
   });
 
   // See https://docs.expo.dev/develop/user-interface/fonts/#use-a-custom-font if this stops working
