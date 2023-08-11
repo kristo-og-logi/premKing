b4f2cc48413163aa729c0abeb847a95af47e58f5
Lsig <logis21@ru.is>
2023-08-11 21:53:47 +0000

    Removed comments

--- frontend/src/components/bet/Confirm.tsx
+++ frontend/src/components/bet/Confirm.tsx
@@ -12,19 +12,19 @@ export const Confirm = () => {
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute', // Position the button absolutely
-    bottom: 15, // Stick it to the bottom
-    left: 40, // Align it to the left (you can adjust this if needed)
-    right: 40, // Align it to the right (you can adjust this if needed)
-    padding: 16, // Add padding if needed
+    position: 'absolute',
+    bottom: 15,
+    left: 40,
+    right: 40,
+    padding: 16,
     backgroundColor: colors.charcoal[2],
-    shadowColor: 'black', // Shadow color
-    shadowOpacity: 0.3, // Shadow opacity (0 to 1)
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
     shadowOffset: {
       width: 0,
       height: 3,
     },
-    shadowRadius: 3, // Shadow radius
+    shadowRadius: 3,
     elevation: 5,
   },
   text: {
