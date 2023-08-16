d4f75f675205e0304742c064d5c36da77aa9cc92
Lsig <logis21@ru.is>
2023-08-16 12:13:31 +0200

    Added more matchups to index in bet

--- frontend/src/app/bet/index.tsx
+++ frontend/src/app/bet/index.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import { View } from 'react-native';
-import { globalStyles } from '../../styles/styles';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import { colors } from '../../styles/styles';
 import { MatchUp } from '../../components/bet/MatchUp';
 import { Gameweek } from '../../components/bet/Gameweek';
 import { Confirm } from '../../components/bet/Confirm';
 
 const Bet = () => {
   return (
-    <View style={globalStyles.container}>
+    <View style={styles.container}>
       <Gameweek />
-      <MatchUp />
-      <MatchUp />
+      <ScrollView>
+        <MatchUp />
+        <MatchUp />
+        <MatchUp />
+        <MatchUp />
+        <MatchUp />
+        <MatchUp />
+        <MatchUp />
+      </ScrollView>
       <Confirm />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.charcoal[1],
+    padding: 16,
+  },
+
+  scrollContainer: {
+    flex: 1,
+    backgroundColor: colors.charcoal[1],
+    padding: 16,
+    height: 100,
+  },
+});
 
 export default Bet;
