fc676d7a217624eaf88e435513f519c6cec038b7
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2023-08-17 21:22:53 +0000

    added backend binary to gitignore

--- server/main.go
+++ server/main.go
@@ -62,12 +62,15 @@ func getCurrentGameWeek(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, 2)
 }
 
-func main() {
+func init() {
 	err := godotenv.Load()
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+func main() {
 
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
@@ -77,5 +80,5 @@ func main() {
 
 	router.GET("/gw", getCurrentGameWeek)
 
-	router.Run("localhost:8080")
+	router.Run()
 }
