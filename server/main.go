6301d28c84292d2e76fbbbd0038d8685f58f1fb8
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2023-08-17 20:43:01 +0000

    added .env package and variables

--- server/main.go
+++ server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 type album struct {
@@ -61,6 +64,13 @@ func getCurrentGameWeek(c *gin.Context) {
 }
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	log.Println(os.Getenv("test"))
+
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
