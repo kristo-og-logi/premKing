ed29acb38a73af46281b74b3906a1960da86ec47
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2023-09-02 20:23:35 +0000

    finally fixed FK Constraint not allowed error
    Turns out PlanetScale doesn't support Foreign Key constraints
    
    see https://planetscale.com/docs/learn/operating
    -without-foreign-key-constraints

--- server/initializers/db.go
+++ server/initializers/db.go
@@ -21,7 +21,9 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	dsn := os.Getenv("DSN")
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
 	if err != nil {
 		log.Fatal("Failed to connect to database ", err)
 	}
--- server/models/League.go
+++ server/models/League.go
@@ -8,7 +8,7 @@ type League struct {
 	gorm.Model
 	ID      string `gorm:"primaryKey"`
 	Name    string
-	OwnerID string
+	OwnerID string `gorm:"not null"`
 	Owner   User   `gorm:"foreignKey:OwnerID"`
 	Users   []User `gorm:"many2many:user_languages;"`
 }
