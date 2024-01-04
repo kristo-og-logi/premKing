e32ec5bf8814fb3cb910d4a9ad5071859994785b
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-01-04 22:05:44 +0000

    tabBar icons dynamically colored

--- frontend/src/app/(app)/_layout.tsx
+++ frontend/src/app/(app)/_layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect, Tabs } from 'expo-router';
 import { Entypo, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { useAppSelector } from '../../redux/hooks';
+import { colors } from '../../styles/styles';
 
 export default function MainLayout() {
   const userSlice = useAppSelector((state) => state.user);
@@ -13,26 +14,30 @@ export default function MainLayout() {
       screenOptions={{
         headerShown: false,
         tabBarStyle: {
-          backgroundColor: '#0A475C',
+          backgroundColor: colors.charcoal[3],
           borderTopWidth: 0,
         },
-        tabBarActiveTintColor: '#fff',
-        tabBarInactiveTintColor: '#aaa',
+        tabBarActiveTintColor: colors.charcoal[12],
+        tabBarInactiveTintColor: colors.charcoal[10],
       }}
     >
       <Tabs.Screen
         name="leagues"
         options={{
-          tabBarIcon: () => <MaterialCommunityIcons name="trophy" size={24} color={'#fff'} />,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="trophy" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="bet"
-        options={{ tabBarIcon: () => <Entypo name="ticket" size={24} color={'#fff'} /> }}
+        options={{ tabBarIcon: ({ color }) => <Entypo name="ticket" size={24} color={color} /> }}
       />
       <Tabs.Screen
         name="stats"
-        options={{ tabBarIcon: () => <Ionicons name="stats-chart" size={24} color={'#fff'} /> }}
+        options={{
+          tabBarIcon: ({ color }) => <Ionicons name="stats-chart" size={24} color={color} />,
+        }}
       />
       <Tabs.Screen name="index" options={{ href: null }} />
     </Tabs>
