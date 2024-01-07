c93436bef57bf7912d80ff8f00c34be99d97fff6
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-01-07 22:56:58 +0000

    cleanup

--- server/models/League.go
+++ server/models/League.go
@@ -2,16 +2,14 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type League struct {
 	// gorm.Model
-	ID        string         `gorm:"primaryKey" json:"id"`
-	CreatedAt time.Time      `json:"createdAt"`
-	UpdatedAt time.Time      `json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
+	ID        string    `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	// DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
 
 	Name    string `json:"name"`
 	OwnerID string `gorm:"not null" json:"ownerId"`
