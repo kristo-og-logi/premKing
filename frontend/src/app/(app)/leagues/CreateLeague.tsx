c061ce9ac76ed007ea5c95f1f82c22a14d7adaf1
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-01-10 23:08:41 +0000

    created similar UX improvements to CreateLeague

--- frontend/src/app/(app)/leagues/CreateLeague.tsx
+++ frontend/src/app/(app)/leagues/CreateLeague.tsx
@@ -1,17 +1,29 @@
 import { View, StyleSheet } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PremText from '../../../components/basic/PremText';
 import { router } from 'expo-router';
 import PremButton from '../../../components/basic/PremButton';
 import PremTextInput from '../../../components/basic/PremTextInput';
-import { globalStyles } from '../../../styles/styles';
+import { colors, globalStyles } from '../../../styles/styles';
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import { createLeague } from '../../../redux/reducers/leaguesReducer';
+import { ERROR_TIMEOUT } from '../../../utils/constants';
+
+const MAX_LEN = 30;
 
 const CreateLeague = () => {
-  const [leagueName, setLeagueName] = useState<string>('');
   const dispatch = useAppDispatch();
   const authSlice = useAppSelector((state) => state.auth);
+
+  const [leagueName, setLeagueName] = useState<string>('');
+  const [inputError, setInputError] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (inputError)
+      setTimeout(() => {
+        setInputError(false);
+      }, ERROR_TIMEOUT);
+  }, [inputError]);
 
   return (
     <View style={globalStyles.container}>
@@ -23,11 +35,13 @@ const CreateLeague = () => {
         <PremTextInput
           placeholder="League name here"
           value={leagueName}
-          onChangeText={(value) => setLeagueName(value)}
+          onChangeText={(value) => {
+            value.length >= MAX_LEN ? setInputError(true) : setLeagueName(value);
+          }}
         />
         <PremButton
+          disabled={leagueName == ''}
           onPress={() => {
-            console.log(`POST /users/me/leagues body: {leagueName: ${leagueName}}`);
             dispatch(createLeague({ token: authSlice.token, leagueName: leagueName }));
             router.back();
           }}
@@ -35,6 +49,11 @@ const CreateLeague = () => {
           Create league
         </PremButton>
       </View>
+      <PremText
+        order={4}
+        centered
+        color={inputError ? colors.red : undefined}
+      >{`League name must be under ${MAX_LEN} characters`}</PremText>
     </View>
   );
 };
