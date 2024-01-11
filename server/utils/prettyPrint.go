d0f2dc2f45e8a41c4d48595464b28f2401e72c8b
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-01-11 09:16:10 +0000

    prettyPrint made prettier

--- server/utils/prettyPrint.go
+++ server/utils/prettyPrint.go
@@ -8,9 +8,13 @@ import (
 
 func PrettyPrint(printString string, printObject any) {
 	prettyJSON, err := json.MarshalIndent(printObject, "", "    ")
+	if !strings.ContainsAny(printString, "%") {
+		printString = strings.Trim(printString, "\n") + "%s\n"
+	}
 	if err == nil {
 		fmt.Printf(strings.Replace(printString, "%+v", "%s", -1), prettyJSON)
 	} else {
+		fmt.Printf("PrettyPrint error: %s\n", err.Error())
 		fmt.Printf(strings.Replace(printString, "%s", "%+v", -1), printObject)
 	}
 }
