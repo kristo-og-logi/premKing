624b4cf678a12f5a72dd08d20e187a435086d6da
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-01-11 17:53:14 +0000

    bet screen loads fixtures

--- frontend/src/app/(app)/bet/index.tsx
+++ frontend/src/app/(app)/bet/index.tsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, ScrollView } from 'react-native';
 import { globalStyles } from '../../../styles/styles';
 import { MatchUp } from '../../../components/bet/MatchUp';
 import { Gameweek } from '../../../components/bet/Gameweek';
 import { Confirm } from '../../../components/bet/Confirm';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import { getFixtures } from '../../../redux/reducers/fixtureReducer';
+import PremText from '../../../components/basic/PremText';
 
 const Bet = () => {
+  const dispatch = useAppDispatch();
+  const fixtureSlice = useAppSelector((state) => state.fixtures);
+
+  useEffect(() => {
+    if (fixtureSlice.fixtures.length == 0) dispatch(getFixtures(10));
+  }, []);
+
+  const renderMatches = () => {
+    return fixtureSlice.fixtures.map((fixture) => <MatchUp key={fixture.id} fixture={fixture} />);
+  };
+
   return (
     <View style={globalStyles.container}>
       <Gameweek />
       <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
-        <MatchUp />
-        <MatchUp />
-        <MatchUp />
-        <MatchUp />
-        <MatchUp />
-        <MatchUp />
-        <MatchUp />
+        {fixtureSlice.isLoading ? (
+          <PremText>loading...</PremText>
+        ) : fixtureSlice.hasError ? (
+          <PremText>Error</PremText>
+        ) : fixtureSlice.fixtures.length == 0 ? (
+          <PremText>no fixture for this gameweek</PremText>
+        ) : (
+          <>{renderMatches()}</>
+        )}
       </ScrollView>
       <Confirm />
     </View>
