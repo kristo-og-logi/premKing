bcf03c039d1f98f36c30066a3fbbb68889549fd7
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-01-22 22:14:13 +0000

    safely store user info in secure storage + logout works

--- frontend/src/app/login/_layout.tsx
+++ frontend/src/app/login/_layout.tsx
@@ -1,29 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Image, TextInput, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Image, View } from 'react-native';
 import { Redirect, useRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as Google from 'expo-auth-session/providers/google';
-import * as SecureStore from 'expo-secure-store';
 
 import { globalStyles } from '../../styles/styles';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import GoogleButton from '../../components/GoogleButton';
-import { login } from '../../redux/reducers/authReducer';
+import { login, setUserDataFromStorage } from '../../redux/reducers/authReducer';
 
 import premkingLogo from '../../../assets/premKingLogo.png';
-import PremButton from '../../components/basic/PremButton';
-import PremText from '../../components/basic/PremText';
-import { getTokenFromStorage, saveTokenInStorage } from '../../utils/storage';
-import PremTextInput from '../../components/basic/PremTextInput';
-
-const deletedValueFor = async (key: string) => {
-  await SecureStore.deleteItemAsync(key);
-  alert('Value deleted');
-};
+import { getTokenFromStorage } from '../../utils/storage';
 
 const Login = () => {
-  const [tokenValue, setTokenValue] = useState<string>('');
-
   const router = useRouter();
   const dispatch = useAppDispatch();
   const authSlice = useAppSelector((state) => state.auth);
@@ -36,9 +25,9 @@ const Login = () => {
     if (response?.type === 'success') {
       console.log('oauth success: ', response.authentication?.accessToken);
 
-      if (response.authentication?.accessToken)
+      if (response.authentication?.accessToken) {
         dispatch(login(response.authentication?.accessToken));
-
+      }
       router.replace('/');
     }
   };
@@ -47,47 +36,26 @@ const Login = () => {
     handleOauth();
   }, [response]);
 
-  if (authSlice.user) return <Redirect href={'/leagues'} />;
+  useEffect(() => {
+    getTokenFromStorage()
+      .then((userData) => {
+        dispatch(setUserDataFromStorage(userData));
+      })
+      .catch((error) => {
+        console.log('token not found in storage');
+      });
+  }, []);
 
-  return (
+  return authSlice.user ? (
+    <Redirect href={'/'} />
+  ) : (
     <SafeAreaView style={{ ...globalStyles.container, justifyContent: 'space-between' }}>
       <View>
         <Image source={premkingLogo} style={{ width: 'auto', height: 200 }} />
       </View>
 
-      {/* <View style={{ display: 'flex', gap: 8 }}>
+      <View style={{ display: 'flex', gap: 8 }}>
         <GoogleButton onPress={() => promptAsync()} />
-      </View> */}
-      <View>
-        <PremTextInput placeholder="token value" onChangeText={setTokenValue} value={tokenValue} />
-        <PremButton
-          onPress={() => {
-            console.log('saving value1');
-            saveTokenInStorage(tokenValue);
-            console.log('saving value2');
-          }}
-        >
-          Save "TOKEN: VALUE"
-        </PremButton>
-        <PremButton
-          onPress={async () => {
-            console.log('getting value1');
-            const token = await getTokenFromStorage();
-            alert(`token is ${token}`);
-            console.log('getting value2');
-          }}
-        >
-          GET "TOKEN"
-        </PremButton>
-        <PremButton
-          onPress={() => {
-            console.log('deleting value1');
-            deletedValueFor('TOKEN');
-            console.log('deleting value2');
-          }}
-        >
-          REMOVE "TOKEN"
-        </PremButton>
       </View>
     </SafeAreaView>
   );
--- frontend/src/utils/storage.ts
+++ frontend/src/utils/storage.ts
@@ -1,11 +1,26 @@
 import * as SecureStore from 'expo-secure-store';
+import { LoginResponse } from '../redux/reducers/authReducer';
+import { jwtDecode } from './jwtDecode';
 
-export const getTokenFromStorage = async (): Promise<string> => {
-  const token = await SecureStore.getItemAsync('TOKEN');
-  if (!token) throw new Error('token not found in storage');
-  return token;
+export const getTokenFromStorage = async (): Promise<LoginResponse> => {
+  const credentials = await SecureStore.getItemAsync('CREDENTIALS');
+  if (!credentials) throw new Error('credentials not found in storage');
+
+  const userData: LoginResponse = JSON.parse(credentials);
+
+  const tokenPayload = jwtDecode(userData.token);
+  const currentTime = Date.now() / 1000;
+  const expiryCounter = tokenPayload.exp - currentTime;
+
+  if (expiryCounter < 0) throw new Error('token expired');
+
+  return userData;
 };
 
-export const saveTokenInStorage = async (token: string) => {
-  await SecureStore.setItemAsync('TOKEN', token);
+export const saveTokenInStorage = async (userData: LoginResponse) => {
+  await SecureStore.setItemAsync('CREDENTIALS', JSON.stringify(userData));
+};
+
+export const removeTokenFromStorage = async () => {
+  await SecureStore.deleteItemAsync('CREDENTIALS');
 };
