f3aa9c2e3be9ecb90770c4e8fe00e7d60fe48747
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-02-20 12:41:15 +0000

    move GetCurrentGameWeek to repository

--- server/controllers/gameweek.go
+++ server/controllers/gameweek.go
@@ -2,31 +2,19 @@ package controllers
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/kristo-og-logi/premKing/server/models"
 	"github.com/kristo-og-logi/premKing/server/repositories"
 )
 
 func GetCurrentGameWeek(c *gin.Context) {
-	gameweeks, err := repositories.GetAllGameWeeks()
-
+	currentGW, err := repositories.GetCurrentGameWeek()
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	var currentGameweek models.Gameweek
-	now := time.Now()
-
-	for index, gw := range gameweeks {
-		if index == len(gameweeks) || gw.Opens.Before(now) && gameweeks[index+1].Opens.After(now) {
-			currentGameweek = gw
-		}
-	}
-
-	c.IndentedJSON(http.StatusOK, currentGameweek)
+	c.IndentedJSON(http.StatusOK, currentGW)
 }
 
 func GetAllGameWeeks(c *gin.Context) {
