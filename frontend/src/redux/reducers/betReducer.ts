d4ad55204e677e65707ba0b842ddb3f278134541
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-02-24 00:06:57 +0000

    submitBet added to redux reducer

--- frontend/src/redux/reducers/betReducer.ts
+++ frontend/src/redux/reducers/betReducer.ts
@@ -8,6 +8,8 @@ export interface BetState {
   selectedGameweek: number;
   isLoading: boolean;
   hasError: boolean;
+  createBetIsLoading: boolean;
+  createBetHasError: boolean;
 }
 
 const initialState: BetState = {
@@ -16,13 +18,15 @@ const initialState: BetState = {
   selectedGameweek: 1,
   isLoading: false,
   hasError: false,
+  createBetIsLoading: false,
+  createBetHasError: false,
 };
 
 export const betSlice = createSlice({
   name: 'bets',
   initialState,
   reducers: {},
-  extraReducers: (builder) =>
+  extraReducers: (builder) => {
     builder
       .addCase(getBets.pending, (state) => {
         state.isLoading = true;
@@ -43,7 +47,26 @@ export const betSlice = createSlice({
           state.bets = action.payload.bets;
           state.selectedGameweek = action.payload.gameweek;
         }
-      ),
+      )
+      // submitBet
+      .addCase(submitBet.pending, (state) => {
+        state.createBetIsLoading = true;
+      })
+      .addCase(submitBet.rejected, (state) => {
+        state.createBetIsLoading = false;
+        state.createBetHasError = true;
+      })
+      .addCase(
+        submitBet.fulfilled,
+        (state, action: PayloadAction<{ createdBets: Bet[]; gameweek: number }>) => {
+          state.createBetIsLoading = false;
+          state.createBetHasError = false;
+          state.bets = action.payload.createdBets;
+          state.notFound = false;
+          state.selectedGameweek = action.payload.gameweek;
+        }
+      );
+  },
 });
 
 interface GetBetsRequest {
@@ -67,8 +90,40 @@ export const getBets = createAsyncThunk<{ bets: Bet[]; gameweek: number }, GetBe
     }
 
     const bets: Bet[] = await response.json();
+    console.log(`Found bets for GW${gameweek}`);
     return { bets, gameweek };
   }
 );
+
+interface SubmitBetRequest {
+  bets: Bet[];
+  gameweek: number;
+  token: string;
+}
+
+export const submitBet = createAsyncThunk<
+  { createdBets: Bet[]; gameweek: number },
+  SubmitBetRequest
+>('fixtures/submitBet', async ({ bets, gameweek, token }) => {
+  console.log(`{ "bets": ${JSON.stringify(bets)} }`);
+  const response = await fetch(`${backend}/users/me/bets/${gameweek}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-type': 'application/json',
+    },
+    body: `{ "bets": ${JSON.stringify(bets)} }`,
+  });
+
+  if (!response.ok) {
+    if (response.status === 404) return { createdBets: [], gameweek };
+    const message: { error: string } = await response.json();
+    console.error(`submitBet ERROR: ${message.error}`);
+    throw new Error(message.error);
+  }
+
+  const createdBets: Bet[] = await response.json();
+  return { createdBets, gameweek };
+});
 
 export default betSlice.reducer;
