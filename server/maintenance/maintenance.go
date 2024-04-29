81e3e7e4545b3a99c8619bdef1bf561af4671a0c
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-04-29 12:06:29 +0000

    server: maintenance updates odds and dates

--- server/maintenance/maintenance.go
+++ server/maintenance/maintenance.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/kristo-og-logi/premKing/server/initializers"
 	"github.com/kristo-og-logi/premKing/server/models"
@@ -20,6 +21,7 @@ type Fixture struct {
 		Value         string `json:"value"`
 		OriginalLabel string `json:"original_label"`
 	} `json:"odds"`
+	StartingAt string `json:"starting_at"`
 }
 
 func main() {
@@ -29,29 +31,74 @@ func main() {
 	dbFixtures := getFixturesFromDB()
 	jsonFixtures := getJSONFixturesFromFile()
 
+	totalUpdated := 0
 	for _, dbfix := range dbFixtures {
 		for _, jsonfix := range jsonFixtures {
 			if dbfix.Name == jsonfix.Name {
-				var homeOdd, drawOdd, awayOdd float32
-				for _, odd := range jsonfix.Odds {
-					if odd.OriginalLabel == "2" {
-						oddValue, _ := strconv.ParseFloat(odd.Value, 32)
-						awayOdd = float32(oddValue)
-					} else if odd.OriginalLabel == "1" {
-						oddValue, _ := strconv.ParseFloat(odd.Value, 32)
-						homeOdd = float32(oddValue)
-					} else if odd.OriginalLabel == "Draw" {
-						oddValue, _ := strconv.ParseFloat(odd.Value, 32)
-						drawOdd = float32(oddValue)
-					} else {
-						panic(fmt.Sprintf("found odd with original_label: %v\n", odd.OriginalLabel))
-					}
+				updated := false
+				updated = assignOdds(dbfix, jsonfix) || updated
+				updated = updateDate(dbfix, jsonfix) || updated
+
+				if updated {
+					totalUpdated++
 				}
-				initializers.DB.Model(&dbfix).Updates(models.Fixture{HomeOdds: homeOdd, DrawOdds: drawOdd, AwayOdds: awayOdd, SportmonksID: jsonfix.Id})
 				break
 			}
 		}
 	}
+	fmt.Printf("Updated %d rows\n", totalUpdated)
+}
+
+func updateDate(dbFixture models.Fixture, jsonFixture Fixture) (updated bool) {
+	updated = false
+
+	jsonTime, err := time.Parse("2006-01-02 15:04:05", jsonFixture.StartingAt)
+	if err != nil {
+		// fmt.Printf("err - %s\n", err.Error())
+		return
+	}
+
+	if jsonTime.Compare(dbFixture.MatchDate) != 0 {
+		// fmt.Printf("%s | from %s to %s\n", dbFixture.Name, dbFixture.MatchDate, jsonTime)
+		initializers.DB.Model(&dbFixture).Updates(models.Fixture{MatchDate: jsonTime})
+		updated = true
+	}
+	return
+}
+
+func assignOdds(dbFixture models.Fixture, jsonFixture Fixture) (updated bool) {
+	updated = false
+	// no need to reassign odds
+	if dbFixture.AwayOdds >= 0.1 || dbFixture.HomeOdds >= 0.1 || dbFixture.DrawOdds >= 0.1 {
+		return
+	}
+	// if odds haven't appeard in API, don't update
+	if len(jsonFixture.Odds) == 0 {
+		return
+	}
+
+	var homeOdd, drawOdd, awayOdd float32
+	for _, odd := range jsonFixture.Odds {
+		if odd.OriginalLabel == "2" {
+			oddValue, _ := strconv.ParseFloat(odd.Value, 32)
+			awayOdd = float32(oddValue)
+		} else if odd.OriginalLabel == "1" {
+			oddValue, _ := strconv.ParseFloat(odd.Value, 32)
+			homeOdd = float32(oddValue)
+		} else if odd.OriginalLabel == "Draw" {
+			oddValue, _ := strconv.ParseFloat(odd.Value, 32)
+			drawOdd = float32(oddValue)
+		} else {
+			panic(fmt.Sprintf("found odd with original_label: %v\n", odd.OriginalLabel))
+		}
+	}
+	initializers.DB.Model(&dbFixture).Updates(models.Fixture{HomeOdds: homeOdd, DrawOdds: drawOdd, AwayOdds: awayOdd})
+	updated = true
+	return
+}
+
+func assignId(dbFixture models.Fixture, jsonFixture Fixture) {
+	initializers.DB.Model(&dbFixture).Updates(models.Fixture{SportmonksID: jsonFixture.Id})
 }
 
 func getFixturesFromDB() []models.Fixture {
