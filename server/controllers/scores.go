49cc1a15a26a60f171526707a6e95ad13b8091e7
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-05-04 00:20:14 +0000

    GET /users/me/scores returns scores

--- server/controllers/scores.go
+++ server/controllers/scores.go
@@ -1,11 +1,20 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kristo-og-logi/premKing/server/repositories"
 	"github.com/kristo-og-logi/premKing/server/utils"
 )
+
+type GetMyScoresResponse struct {
+	Gameweek int     `json:"gameweek"`
+	Score    float64 `json:"score"`
+	Total    float64 `json:"total"`
+}
 
 func GetMyScores(c *gin.Context) {
 	user := utils.GetUserFromContext(c)
@@ -14,5 +23,39 @@ func GetMyScores(c *gin.Context) {
 		return
 	}
 
-	c.IndentedJSON(http.StatusOK, gin.H{"hello": "world"})
+	bets, err := repositories.GetAllBetsById(user.ID)
+	if err != nil {
+		fmt.Printf("Error fetching bets from user with ID %v: %s", user.ID, err.Error())
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
+
+	sort.Slice(bets, func(i, j int) bool {
+		return bets[i].GameWeek <= bets[j].GameWeek
+	})
+
+	resp := []GetMyScoresResponse{}
+
+	totalScore := 0.0
+	for gw := 1; gw <= 38; gw++ {
+		gwScore := 0.0
+		for _, bet := range bets {
+			if int(bet.GameWeek) != gw {
+				continue
+			}
+			if bet.Won {
+				gwScore += float64(bet.Odd)
+			}
+		}
+
+		resp = append(resp, GetMyScoresResponse{
+			Gameweek: gw,
+			Score:    gwScore,
+			Total:    totalScore + gwScore,
+		})
+		totalScore += gwScore
+
+	}
+
+	c.IndentedJSON(http.StatusOK, resp)
 }
