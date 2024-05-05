497928ddd38829a942732806cd3f25c2413df3f4
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-05-05 16:44:22 +0000

    created Intl date formatter

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -4,3 +4,12 @@ export const CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 export const ID_LEN = 6;
 export const ERROR_TIMEOUT = 3000;
 export const backend = `${BACKEND_URL}/api/v1`;
+
+export const dateFormatter = new Intl.DateTimeFormat('en-US', {
+  weekday: 'short',
+  month: 'short',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false,
+});
