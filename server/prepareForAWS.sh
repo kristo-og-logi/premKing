29ad678edbb19f2dd6d4c0aaa91b337a3fb927fa
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-01-12 22:40:22 +0000

    exclude logs/ dir from aws zip

--- server/prepareForAWS.sh
+++ server/prepareForAWS.sh
@@ -8,7 +8,7 @@ GOOS=linux GOARCH=amd64 go build -o awsServer
 
 rm ../aws.zip
 # zip the repository into a server.zip
-zip -r ../aws.zip * ".env.PROD" ".platform"
+zip -r ../aws.zip * ".env.PROD" ".platform" -x "logs/*"
 
 echo "New aws.zip has been created in parent directory"
 echo "You may upload it to AWS"
