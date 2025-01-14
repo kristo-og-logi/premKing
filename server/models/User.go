1ca9bbaf483eaf1cf09e514ff2719065bab81973
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-01-14 00:41:14 +0000

    add private ExpoPushToken column to User table

--- server/models/User.go
+++ server/models/User.go
@@ -13,9 +13,10 @@ type User struct {
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
 
-	Name     string   `json:"name"`
-	Username string   `json:"username"`
-	Email    string   `gorm:"type:varchar(255);uniqueIndex;not null" json:"email"`
-	Leagues  []League `gorm:"many2many:user_leagues;" json:"leagues"`
-	AppleId  string   `gorm:"index" json:"-"` // used for users signing in with apple
+	Name          string   `json:"name"`
+	Username      string   `json:"username"`
+	Email         string   `gorm:"type:varchar(255);uniqueIndex;not null" json:"email"`
+	Leagues       []League `gorm:"many2many:user_leagues;" json:"leagues"`
+	AppleId       string   `gorm:"index" json:"-"` // used for users signing in with apple
+	ExpoPushToken string   `json:"-"`              // we do not want to expose our expo push tokens
 }
