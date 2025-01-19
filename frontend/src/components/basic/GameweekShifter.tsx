f468e195ba0738a41f3f879e286e8ffdde81dd06
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-01-19 16:41:26 -0500

    change GW shifter color on pressed

--- frontend/src/components/basic/GameweekShifter.tsx
+++ frontend/src/components/basic/GameweekShifter.tsx
@@ -18,19 +18,25 @@ const GameweekShifter = ({ selectedGW, setSelectedGameweek }: Props) => {
         <Pressable
           style={[styles.shifter]}
           onPress={() => {
-            if (selectedGW > 1) setSelectedGameweek(selectedGW - 1);
+            setSelectedGameweek(selectedGW - 1);
           }}
+          disabled={selectedGW <= 1}
         >
-          <AntDesign name="left" size={32} color={selectedGW > 1 ? colors.gray[0] : colors.gray[2]} />
+          {({ pressed }) => (
+            <AntDesign name="left" size={32} color={selectedGW > 1 && !pressed ? colors.gray[0] : colors.gray[3]} />
+          )}
         </Pressable>
         <PremText order={1} centered>{`Gameweek ${selectedGW}`}</PremText>
         <Pressable
           style={[styles.shifter]}
           onPress={() => {
-            if (selectedGW < 38) setSelectedGameweek(selectedGW + 1);
+            setSelectedGameweek(selectedGW + 1);
           }}
+          disabled={selectedGW >= 38}
         >
-          <AntDesign name="right" size={32} color={selectedGW < 38 ? colors.gray[0] : colors.gray[2]} />
+          {({ pressed }) => (
+            <AntDesign name="right" size={32} color={selectedGW < 38 && !pressed ? colors.gray[0] : colors.gray[3]} />
+          )}
         </Pressable>
       </View>
 
