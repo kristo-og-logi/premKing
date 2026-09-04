43beab4c139cdfef96e8f752903bbaa69ed65df3
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2026-09-03 22:53:15 -0400

    cleanup migration runner

--- server/initializers/db.go
+++ server/initializers/db.go
@@ -1,21 +1,18 @@
 package initializers
 
 import (
-	"fmt"
 	"log"
 	"log/slog"
 	"os"
-	"strings"
 	"time"
 
-	"github.com/kristo-og-logi/premKing/server/external"
 	"github.com/kristo-og-logi/premKing/server/models"
-	"github.com/kristo-og-logi/premKing/server/utils"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
+// Setup/Update DB tables to match Go structs
 func autoMigrateDB(db *gorm.DB) {
 	err := db.AutoMigrate(&models.League{}, &models.User{}, &models.Fixture{}, &models.Team{}, &models.Gameweek{}, &models.Bet{})
 	if err != nil {
@@ -52,13 +49,11 @@ func ConnectDB() {
 	dbName := db.Migrator().CurrentDatabase()
 	slog.Debug("Connected to DB", "dbname", dbName, "name", db.Name())
 
-	shouldMigrate := true
+	shouldMigrate := false
 	if shouldMigrate {
 		slog.Info("migrating")
 		autoMigrateDB(db)
-		migrateTeamsToDB(db)
-		migrateFixturesToDB(db)
-		migrateGameweeksToDB(db)
+		MigrateGameweeksToDB(db)
 	} else {
 		slog.Info("not migrating")
 	}
@@ -66,147 +61,9 @@ func ConnectDB() {
 	DB = db
 }
 
-func migrateTeamsToDB(db *gorm.DB) {
-	var existingTeams []models.Team
-	result := db.Select("id").Find(&existingTeams)
-	if result.Error != nil {
-		slog.Error("error fetching existing teams", "error", result.Error.Error())
-	}
-
-	if len(existingTeams) >= 20 {
-		slog.Info("all teams already existing in db. Stopping")
-		return
-	}
-
-	teamsData := external.GetRapidApiTeamsResponse().Response
-
-	for _, team := range teamsData {
-		name := utils.ConvertTeamName(team.Team.Name)
-		model := models.Team{
-			ID:        team.Team.ID,
-			Name:      name,
-			ShortName: utils.GetShortName(name),
-			Logo:      team.Team.Logo,
-		}
-
-		exists := false
-		for _, existingTeam := range existingTeams {
-			if existingTeam.ID == model.ID {
-				exists = true
-			}
-		}
-		if !exists {
-			result := db.Where(models.Team{ID: model.ID}).FirstOrCreate(&model)
-			slog.Info(fmt.Sprintf("added %s", team.Team.Name))
-			if result.Error != nil {
-				slog.Error("Error adding team to DB", "teamName", team.Team.Name, "error", result.Error.Error())
-				os.Exit(1)
-			}
-		} else {
-			slog.Info("already exists", "team", model.Name)
-		}
-	}
-}
-
-func migrateFixturesToDB(db *gorm.DB) {
-	var dbFixtures []models.Fixture
-	result := db.Select("id").Find(&dbFixtures)
-	if result.Error != nil {
-		slog.Error("error fetching existing fixtures", "error", result.Error.Error())
-		os.Exit(1)
-	}
-
-	if len(dbFixtures) >= 380 {
-		slog.Info("all fixtures already exist in db. Stopping")
-		return
-	}
-
-	var teams []models.Team
-	teamResult := db.Find(&teams)
-	if teamResult.Error != nil {
-		slog.Error("error fetching all teams", "error", result.Error.Error())
-		os.Exit(1)
-	}
-
-	rapidFixtures := external.GetRapidApiFixturesResponse().Response
-	slog.Info(fmt.Sprintf("got %d fixtures from rapidAPI", len(rapidFixtures)))
-
-	sportmonksFixtures := external.FetchSportmonksFixtures()
-
-	for _, fixture := range rapidFixtures {
-
-		exists := false
-		for _, dbFixture := range dbFixtures {
-			if dbFixture.ID == fixture.Fixture.ID {
-				exists = true
-			}
-		}
-		if !exists {
-			var homeTeam models.Team
-			var awayTeam models.Team
-			for _, team := range teams {
-				if team.ID == fixture.Teams.Home.ID {
-					homeTeam = team
-				}
-				if team.ID == fixture.Teams.Away.ID {
-					awayTeam = team
-				}
-			}
-
-			sportmonksID := -fixture.Fixture.ID
-			for _, sFix := range sportmonksFixtures {
-				teams := strings.Split(sFix.Name, " vs ")
-				home, away := teams[0], teams[1]
-
-				if home == homeTeam.Name && away == awayTeam.Name {
-					sportmonksID = sFix.Id
-				}
-			}
-
-			fixtureName := utils.CreateFixtureName(homeTeam, awayTeam)
-			if sportmonksID < 0 {
-				slog.Error("Could not find a sportmonksID for fixture", "FixtureID", fixture.Fixture.ID, "Name", fixtureName)
-			}
-
-			fixtureResult := "X"
-			if fixture.Teams.Home.Winner {
-				fixtureResult = "1"
-			} else if fixture.Teams.Away.Winner {
-				fixtureResult = "2"
-			}
-
-			model := models.Fixture{
-				ID:           fixture.Fixture.ID,
-				CreatedAt:    time.Now(),
-				UpdatedAt:    time.Now(),
-				HomeTeamId:   homeTeam.ID,
-				HomeTeam:     homeTeam,
-				AwayTeamId:   awayTeam.ID,
-				AwayTeam:     awayTeam,
-				Finished:     fixture.Fixture.Status.Elapsed == 90,
-				HomeGoals:    fixture.Goals.Home,
-				AwayGoals:    fixture.Goals.Away,
-				Result:       fixtureResult,
-				MatchDate:    fixture.Fixture.Date,
-				GameWeek:     utils.GetGameweekFromRound(fixture.League.Round),
-				Name:         fixtureName,
-				LongName:     fmt.Sprintf("%s vs %s", homeTeam.Name, awayTeam.Name),
-				SportmonksID: sportmonksID,
-				IsNormal:     true,
-			}
-
-			result := db.Where(models.Fixture{ID: model.ID}).FirstOrCreate(&model)
-			if result.Error != nil {
-				slog.Error("Error adding fixture to DB", "fixtureName", model.Name, "homeTeamId", homeTeam.ID, "awayTeamId", awayTeam.ID, "error", result.Error.Error())
-				os.Exit(1)
-			}
-		}
-	}
-
-	slog.Info("Successfully added all fixtures")
-}
-
-func migrateGameweeksToDB(db *gorm.DB) {
+// If gameweek table isn't full, create gameweeks using fixtures
+// This does not consider "Normal" fixtures, that's done in package `crons`
+func MigrateGameweeksToDB(db *gorm.DB) {
 	type FirstAndLastFixture struct {
 		GameWeek         uint8     `gorm:"game_week"`
 		FirstFixtureDate time.Time `gorm:"first_fixture_date"`
